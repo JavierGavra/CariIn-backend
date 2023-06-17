78a59d2ef2122b6c31bff8366f1e5c9fce013e84
JavierGavra <javiergavra01@gmail.com>
2023-06-17 10:10:57 +0700

    fear : [JG] refresh token

--- app/Http/Controllers/RefreshTokenController.php
+++ app/Http/Controllers/RefreshTokenController.php
@@ -8,37 +8,16 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class RefreshTokenController extends Controller
 {
-    public function refreshToken(Request $request)
+    public function refreshToken()
     {
-        $user = Auth::guard($request->role)->user();
         $newToken = JWTAuth::refresh(JWTAuth::getToken());
-
-        if ($request->role == 'admin' || $request->role == 'worker') {
-            return response()->json([
-                'success' => true,
-                'message' => 'Successfully refresh token',
-                'data' => [
-                    'user' => [
-                        'email' => $user->email,
-                        'username' => $user->username,
-                    ],
-                    'role' => $user->role,
-                    'token' => $newToken,
-                ]
-            ]);
-        } elseif ($request->role == 'company') {
-            return response()->json([
-                'success' => true,
-                'message' => 'Successfully refresh token',
-                'data' => [
-                    'user' => [
-                        'email' => $user->email,
-                        'name' => $user->name,
-                    ],
-                    'role' => $user->role,
-                    'token' => $newToken,
-                ]
-            ]);
-        }
+        return response()->json([
+            'success' => true,
+            'message' => 'Successfully refresh token',
+            'data' => [
+                'token' => $newToken,
+            ]
+        ]);
     }
 }
+
