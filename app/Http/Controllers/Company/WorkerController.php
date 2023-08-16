2659d153304a9588b18ed447cd34d44dacf08221
JavierGavra <javiergavra01@gmail.com>
2023-08-16 11:35:09 +0700

    feat: [JG] add route for worker detail on company

--- app/Http/Controllers/Company/WorkerController.php
+++ app/Http/Controllers/Company/WorkerController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Company;
 
 use App\Helpers\AppFunction;
+use App\Helpers\HttpStatus;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Experience\ExperienceDetailResource;
+use App\Http\Resources\Experience\ExperienceListResource;
+use App\Http\Resources\Skill\SkillResource;
 use App\Http\Resources\Worker\WorkerDetailResource;
 use App\Http\Resources\Worker\WorkerListResource;
 use App\Models\Worker;
@@ -22,21 +26,63 @@ class WorkerController extends Controller
     }
 
     public function show(int $id) {
-        $workers = Worker::where('company_visible', 1)->find($id);
-
-        if (is_null($workers)) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Data not found',
-                'data' => [],
-            ], 404);
+        $worker = Worker::where('company_visible', 1)->find($id);
+        
+        if (is_null($worker)) {
+            return HttpStatus::code404("Data not found");
         } else {
             return response()->json([
                 'success' => true,
                 'message' => 'Data found',
-                'data' => new WorkerDetailResource($workers),
+                'data' => new WorkerDetailResource($worker),
             ]);
         }
+    }
+    
+    public function getExperiences(int $id) {
+        $worker = Worker::where('company_visible', 1)->find($id);
         
+        if (is_null($worker)) {
+            return HttpStatus::code404("Data not found");
+        } else {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data found',
+                'data' => ExperienceListResource::collection($worker->experiences),
+            ]);
+        }
+    }
+    
+    public function showExperience(int $id, int $experience_id) {
+        $worker = Worker::where('company_visible', 1)->find($id);
+        if (is_null($worker)) {
+            return HttpStatus::code404("Data not found");
+        }
+
+        $experience = $worker->experiences->find($experience_id);
+        if (is_null($experience)) {
+            return HttpStatus::code404("Data not found");
+        }
+        
+        return response()->json([
+            'success' => true,
+            'message' => 'Data found',
+            'data' =>new ExperienceDetailResource($experience),
+        ]);
+        
+    }
+
+    public function getSkills(int $id) {
+        $worker = Worker::where('company_visible', 1)->find($id);
+        
+        if (is_null($worker)) {
+            return HttpStatus::code404("Data not found");
+        } else {
+            return response()->json([
+                'success' => true,
+                'message' => 'Data found',
+                'data' => SkillResource::collection($worker->skills),
+            ]);
+        }
     }
 }
